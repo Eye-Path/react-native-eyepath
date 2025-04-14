2844d8184f63b4698b371f2a35f0120d9ce2ab00
ummm <mh080411@kyonggi.ac.kr>
2025-04-14 16:32:56 +0900

    초기 세팅

---
+++ eyepath_project/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to Eyepath</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+    fontSize: '100px',
+  },
+});
+
+export default App;
+
