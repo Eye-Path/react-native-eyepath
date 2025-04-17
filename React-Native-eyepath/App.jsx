35a7d66cb9cc89fedee96626c8fe2ff48f0e8f81
ummm <mh080411@kyonggi.ac.kr>
2025-04-17 16:07:05 +0900

    메인 update

---
+++ React-Native-eyepath/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+});
+export default App;
