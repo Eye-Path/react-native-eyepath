fe3879f14c85fc95fedaf6967f4d7e7ca46619e1
avalance4114 <avalance4114@gmail.com>
2025-06-02 08:31:17 +0900

    Update NavigationRoutePage_2.jsx
    Tmap 배경 추가

--- React-Native-eyepath/pages/NavigationRoutePage_2.jsx
+++ React-Native-eyepath/pages/NavigationRoutePage_2.jsx
@@ -1,42 +1,39 @@
 import * as React from 'react';
 import {StyleSheet, ScrollView, View} from 'react-native';
-import {useState} from 'react';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 import InfoBox from '../components/NavigationRoutePage_2/InfoBox/InfoBox';
 import BottomSection from '../components/NavigationRoutePage_2/BottomSection/BottomSection';
 import MicSection from '../components/NavigationRoutePage_2/MicSection/MicSection';
-import CameraScreen from '../components/NavigationRoutePage_2/CameraScreen/CameraScreen';
-import Camera from '../components/NavigationRoutePage_2/CameraScreen/Camera';
-
-const queryClient = new QueryClient();
+import TmapPage from '../components/NavigationPage/TmapPage';
 
 const NavigationRoutePage_2 = () => {
-  const [photoPath, setPhotoPath] = useState(null);
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <ScrollView contentContainerStyle={styles.background}>
+    <View style={styles.container}>
+      <View style={StyleSheet.absoluteFill}>
+        <TmapPage />
+      </View>
+
+      <ScrollView contentContainerStyle={styles.contentOverlay}>
         <View>
           <InfoBox />
           <MicSection />
         </View>
-        <CameraScreen setPhotoPath={setPhotoPath} />
-        <Camera photoPath={photoPath} />
         <BottomSection />
       </ScrollView>
-    </QueryClientProvider>
+    </View>
   );
 };
 
 export default NavigationRoutePage_2;
 
 const styles = StyleSheet.create({
-  background: {
-    display: 'flex',
-    justifyContent: 'space-between',
+  container: {
     flex: 1,
-    backgroundColor: '#F8F7FF',
+  },
+  contentOverlay: {
+    flexGrow: 1,
+    backgroundColor: 'transparent',
+    justifyContent: 'space-between',
     paddingTop: 50,
     paddingLeft: 32,
     paddingRight: 32,
