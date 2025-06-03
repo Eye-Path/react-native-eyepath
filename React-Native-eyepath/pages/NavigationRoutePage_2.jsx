245c24aa54f2526d9db188b48d55e7cd9753b424
hazimete <nim1300@kyonggi.ac.kr>
2025-06-04 05:56:45 +0900

    Update NavigationRoutePage_2.jsx

--- React-Native-eyepath/pages/NavigationRoutePage_2.jsx
+++ React-Native-eyepath/pages/NavigationRoutePage_2.jsx
@@ -4,39 +4,31 @@ import {StyleSheet, ScrollView, View} from 'react-native';
 import InfoBox from '../components/NavigationRoutePage_2/InfoBox/InfoBox';
 import BottomSection from '../components/NavigationRoutePage_2/BottomSection/BottomSection';
 import MicSection from '../components/NavigationRoutePage_2/MicSection/MicSection';
-import TmapPage from '../components/NavigationPage/TmapPage';
 
 const NavigationRoutePage_2 = () => {
   return (
-    <View style={styles.container}>
-      <View style={StyleSheet.absoluteFill}>
-        <TmapPage />
+    <ScrollView contentContainerStyle={styles.background}>
+      <View>
+        <InfoBox />
+        <MicSection />
       </View>
-
-      <ScrollView contentContainerStyle={styles.contentOverlay}>
-        <View>
-          <InfoBox />
-          <MicSection />
-        </View>
-        <BottomSection />
-      </ScrollView>
-    </View>
+      <BottomSection />
+    </ScrollView>
   );
 };
 
 export default NavigationRoutePage_2;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  contentOverlay: {
-    flexGrow: 1,
-    backgroundColor: 'transparent',
+  background: {
+    display: 'flex',
     justifyContent: 'space-between',
+    flex: 1,
+    backgroundColor: '#F8F7FF',
     paddingTop: 50,
     paddingLeft: 32,
     paddingRight: 32,
     paddingBottom: 50,
   },
 });
+
